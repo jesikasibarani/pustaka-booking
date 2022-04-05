9ce099856ef2dfb50e2feba88c5cd778b4eb0cf7
jesikasibarani <jesikasibarani123@gmail.com>
2022-04-06 01:12:34 +0700

    menambahkan v_header.php kedalam application/views

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -16,7 +16,7 @@
                     <ul> 
                         <li><a href="<?php echo base_url().'index.php/web' ?>">Home</a></li> 
                     <li><a href="<?php echo base_url().'index.php/web/about' ?>">About</a></li> 
-                </ul> 
-            </nav> 
-            <div class="clear"></div> 
-        </header>
+                    </ul> 
+                </nav> 
+        <div class="clear"></div> 
+           </header>
